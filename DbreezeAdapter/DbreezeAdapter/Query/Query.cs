494f85c955e0f4099f522f11b23f8c01a0b8dcfa
Lokesh Lal <lokeshlal@gmail.com>
2019-02-01 11:14:31 +0530

    removed params function for And and Or

--- DbreezeAdapter/DbreezeAdapter/Query/Query.cs
+++ DbreezeAdapter/DbreezeAdapter/Query/Query.cs
@@ -24,31 +24,9 @@ namespace DbreezeAdapter
             return new QueryAnd(left, right);
         }
 
-        public static Query And(params Query[] queries)
-        {
-            if (queries == null || queries.Length < 2) throw new Exception("At least two Query should be passed");
-            var left = queries[0];
-            for (int i = 1; i < queries.Length; i++)
-            {
-                left = And(left, queries[i]);
-            }
-            return left;
-        }
-
         public static Query Or(Query left, Query right)
         {
             return new QueryOr(left, right);
-        }
-
-        public static Query Or(params Query[] queries)
-        {
-            if (queries == null || queries.Length < 2) throw new Exception("At least two Query should be passed");
-            var left = queries[0];
-            for (int i = 1; i < queries.Length; i++)
-            {
-                left = Or(left, queries[i]);
-            }
-            return left;
         }
 
         public static Query Eq(string field, DbreezeValue value)
